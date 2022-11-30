47a5c1b4dd5bbce770c35eaf71cb302577109abd
pujiragil <ragilsg164@gmail.com>
2022-11-30 15:58:57 +0700

    create grid dislpay for layout component

--- layout/layout.js
+++ layout/layout.js
@@ -1,8 +1,16 @@
 export default function Layout({children}) {
   return (
-    <div className="container mx-auto">
-      <h1>Layout</h1>
-      {children}
+    <div className="flex h-screen bg-blue-400">
+      <div className="m-auto bg-slate-50 rounded-md w-3/5 h-3/4 grid lg:grid-cols-2">
+          <div>
+
+          </div>
+          <div className="right flex flex-col justify-evenly">
+            <div className="text-center py-10">
+              {children}
+            </div>
+          </div>
+      </div>
     </div>
   )
 }
