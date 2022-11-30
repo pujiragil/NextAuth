859c770b21f63facaf51d1f7ce83e72baac8f627
pujiragil <ragilsg164@gmail.com>
2022-11-30 15:53:28 +0700

    create layout components

---
+++ layout/layout.js
@@ -0,0 +1,8 @@
+export default function Layout({children}) {
+  return (
+    <div className="container mx-auto">
+      <h1>Layout</h1>
+      {children}
+    </div>
+  )
+}
--- pages/login.js
+++ pages/login.js
@@ -1,12 +1,13 @@
 import Head from "next/head";
+import Layout from "../layout/layout";
 
 export default function Login() {
   return (
-    <div className="container mx-auto h-screen flex items-center justify-center">
+    <Layout>
       <Head>
         <title>Login</title>
       </Head>
       <h1 className="text-3xl font-semibold">Login Page</h1>
-    </div>
-  )
+    </Layout>
+  );
 }
